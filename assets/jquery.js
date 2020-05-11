9ef83348af2291672fc380890489b7a7fe32e9ff
fizen1201 <62836864+fizen1201@users.noreply.github.com>
2020-05-11 17:07:33 +0530

    Add files via upload

---
+++ assets/jquery.js
@@ -0,0 +1,10 @@
+$('#booton').on("click", function(){
+	$('#m').fadeToggle(1200);
+	$('#mountgif').fadeToggle(2500);
+
+});
+$('#mountgif').on("click", function(){
+	$('#mountain').slideToggle(2200);
+
+
+});
